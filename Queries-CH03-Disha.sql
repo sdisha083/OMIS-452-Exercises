c51ec23f9cc99e58d30be07d834a0a983210e8fc
Disha <55199587+sdisha083@users.noreply.github.com>
2019-10-10 23:25:09 -0500

    Query-CH03-53 by Disha

--- Queries-CH03-Disha.sql
+++ Queries-CH03-Disha.sql
@@ -246,3 +246,11 @@ ORDER BY P.ProjectID, A.EmployeeNumber;
 
  /* *** SQL-Query-CH03-52 *** */
 SELECT * FROM PROJECT;
+
+ /* *** SQL-Query-CH03-53 *** */
+SELECT ProjectName, FirstName, LastName, HoursWorked
+FROM EMPLOYEE AS E JOIN ASSIGNMENT AS A
+ON E.EmployeeNumber = A.EmployeeNumber
+JOIN PROJECT AS P
+ON A.ProjectID = P.ProjectID
+ORDER BY P.ProjectID, A.EmployeeNumber;
